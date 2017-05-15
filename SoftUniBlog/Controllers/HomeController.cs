fcf41219e580665713e16310cc6cf1d1e4314201
DDimitrova90 <ddimitrova90@gmail.com>
2017-05-15 19:41:25 +0300

    Upload files

---
+++ SoftUniBlog/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using SoftUniBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace SoftUniBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("ListCategories");
+        }
+
+        public ActionResult ListCategories()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var categories = database.Categories
+                    .Include(c => c.Articles)
+                    .OrderBy(c => c.Name)
+                    .ToList();
+
+                return View(categories);
+            }
+        }
+
+        public ActionResult ListArticles(int? categoryId, int? page)
+        {
+            if (categoryId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var articles = database.Articles
+                    .Where(a => a.CategoryId == categoryId)
+                    .Include(a => a.Author)
+                    .Include(a => a.Tags);
+
+                int pageSize = 3;
+                int pageNumber = (page ?? 1);
+
+                return View(articles.OrderBy(a => a.Title).ToPagedList(pageNumber, pageSize));
+            }
+        }
+    }
+}
